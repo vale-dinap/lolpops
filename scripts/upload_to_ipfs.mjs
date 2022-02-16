8eb6bf6456565140b5353f6e34d8c36e93cad56d
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-02-16 10:48:06 +0100

    Create upload_to_ipfs.mjs
    Script allowing to upload multiple files to IPFS by using nft.storage as hosting service.

---
+++ scripts/upload_to_ipfs.mjs
@@ -0,0 +1,44 @@
+import fs from 'fs'
+import { NFTStorage, File, Blob } from 'nft.storage'
+
+const basePath = "C:\\Users\\valer\\Desktop\\LOLPOPS\\LOLPOPS"
+const endpoint = 'https://api.nft.storage' // the default
+const token = '' // your API key from https://nft.storage/manage
+
+async function store(basePath, id) {
+    let storage = new NFTStorage({ endpoint, token })
+    let data = await fs.promises.readFile(basePath+"\\IMG\\"+id+".png")
+    let cid = await storage.storeBlob(new Blob([data]))
+    console.log({ cid })
+    let status = await storage.status(cid)
+    console.log(status)
+    return cid;
+  }
+
+async function setURI(basePath, id, cid) {
+    let jsonFile = basePath+"\\JSON\\"+id+".json"
+    fs.readFile(jsonFile, 'utf8', (error, data) => {
+        if(error){
+           console.log(error);
+           return;
+        }
+        let jsonData=JSON.parse(data);
+        jsonData.image = "https://ipfs.io/ipfs/"+cid;
+        let stringData = JSON.stringify(jsonData, null, 4);
+        fs.writeFile(jsonFile, stringData, (err) => {
+            if (err) {
+                throw err;
+            }
+            //console.log("JSON data is saved.");
+        });
+    })
+}
+
+async function uploadAll(base_path, iterations){
+    for(let i=1;i<=iterations;i++){
+        let cid = await store(base_path, i);
+        setURI(base_path, i, cid);
+    }
+}
+
+uploadAll(basePath, 100)
