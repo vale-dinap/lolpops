49650168b55b2be5a1b5c2b4d40091bbd2a9ef01
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-02-28 22:28:14 +0100

    Added all contracts
    The migration file now includes all of the contracts

--- migrations/2_POPS_migration.js
+++ migrations/2_POPS_migration.js
@@ -1,8 +1,14 @@
 const POPSteamWallet = artifacts.require("POPSteamWallet");
 const POPS = artifacts.require("lolpops");
+const POPSsale = artifacts.require("POPSsale");
+const POPSgoldenTicket = artifacts.require("POPSgoldenTickets");
 
 module.exports = async function (deployer) {
   await deployer.deploy(POPSteamWallet, "LOLPOPS team shares", "POPSshares");
   let walletContract = await POPSteamWallet.deployed();
   await deployer.deploy(POPS, "LOLPOPS", "POPS", 10000, walletContract.address);
+  let POPScontract = await POPS.deployed();
+  await deployer.deploy(POPSsale, POPScontract.address, walletContract.address);
+  let POPSsaleContract = await POPSsale.deployed();
+  await deployer.deploy(POPSgoldenTicket, "LOLPOPS golden ticket", "POPSGT", POPSsaleContract.address, POPScontract.address);
 };
