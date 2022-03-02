2df5e345d816804eb52693f087096fd24fe2981d
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-03-02 17:35:46 +0100

    Create merkle_tree.js
    Script to generate the merkle tree of the addresses whitelist

---
+++ scripts/merkle_tree.js
@@ -0,0 +1,19 @@
+// Generate merkle tree
+
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+
+// Replace contents with address list
+let whitelist = [
+    "adress1",
+    "adress2",
+    "adress3",
+    "..." 
+];
+
+const leafNodes = whitelist.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree( leafNodes, keccak256, {sortPairs: true});
+const rootHash = merkleTree.getRoot();
+
+console.log(leafNodes);
+console.log(merkleTree);
