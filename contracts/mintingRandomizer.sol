b457c26614f6631b6860fbd311e5ef31954babb5
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-03-02 17:34:09 +0100

    Optimizations
    Optimizations. Requires testing.

--- contracts/mintingRandomizer.sol
+++ contracts/mintingRandomizer.sol
@@ -9,36 +9,42 @@ pragma solidity ^0.8.0;
 contract MintingRandomizer{
 
     // VARIABLES
-    uint64 private rollId;                                                                         // Adds entropy by keeping track of the current dice roll
-    mapping(uint8 => address) private minter;                                                      // The draw is randomized by using the last 3 minter addresses as source of entropy
     uint8 private nextMinterId = 0;                                                                // Each of the last 3 minters is assigned an id from 0 to 2 (used cyclically)
-    uint256[10] private mintableIds = [0,1,2,3,4,5,6,7,8,9];                                       // Initialize the array containing the final metadata ids, this is randomized at each draw
-    uint256 private nextMintableId=10;                                                             // Value that will replace the just-minted ID in the mintableId array
+    uint8 private rollId;                                                                          // Adds entropy by keeping track of the current dice roll
+    uint16 private nextMintableId=10;                                                              // Value that will replace the just-minted ID in the mintableId array
+    uint16[] private mintableIds = [0,1,2,3,4,5,6,7,8,9];                                          // Initialize the array containing the final metadata ids, this is randomized at each draw
     mapping(uint256 => uint256) internal minterRequestBlock;                                       // Keeps track of when a minter has requested a draw // TODO: consider moving to derived contract
+    mapping(uint8 => address) private minter;                                                      // The draw is randomized by using the last 3 minter addresses as source of entropy
 
 
     // FUNCTIONS - Main logics
 
     // Fetch a metadata ID from the array and replace it with next available
-    function _useMintId(uint array_pos, uint256 max_supply) internal returns(uint256 usedId){
+    function _useMintId(uint array_pos, uint16 max_supply) internal returns(uint16 usedId){
         usedId = mintableIds[array_pos];
-        mintableIds[array_pos] = nextMintableId;
-        nextMintableId = (nextMintableId >= max_supply ? max_supply : nextMintableId+1);
+        if(nextMintableId < max_supply){
+            mintableIds[array_pos] = nextMintableId;
+            nextMintableId++;
+        }
+        else{
+            mintableIds[array_pos] = mintableIds[mintableIds.length-1];
+            mintableIds.pop();
+        }
     }
 
     // Random function using the addresses of trhe last few minters as source of entropy
     function _shuffle() internal returns(bool success) {
-        for (uint256 i = 0; i < mintableIds.length; i++) {
+        for (uint16 i = 0; i < mintableIds.length; i++) {
             uint256 n = i + uint256(keccak256(
                 abi.encodePacked(   rollId,
                                     minter[0], minter[1], minter[2],                               // Additional entropy from addresses of the last 3 minters
-                                    blockhash(block.number)
+                                    block.timestamp
                 ))) % (mintableIds.length - i);
-            uint256 temp = mintableIds[n];
+            uint16 temp = mintableIds[n];
             mintableIds[n] = mintableIds[i];
             mintableIds[i] = temp;
         }
-        rollId+=1;                                                                                 // Increase the roll ID to make sure that no draw gives the same result as the previous
+        unchecked {rollId++;}                                                                      // Increase the roll ID to make sure that no draw gives the same result as the previous
         success=true;
     }
 
@@ -65,9 +71,7 @@ contract MintingRandomizer{
         nextMinterId = (nextMinterId==2 ? 0 : nextMinterId+1);
     }
 
-
-
-    function getMintableIds() view public returns(uint256[10] memory){ //// TESTING ONLY - DELETE THIS AFTERWARDS
+    function getMintableIds() view public returns(uint16[] memory){ /////////////////////////////////// TEST ONLY- DELETE IT ///////////////////////////////////////
         return(mintableIds);
     }
 
