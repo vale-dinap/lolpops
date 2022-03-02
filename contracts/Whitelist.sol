c04fae48be83c8ff4e82d76e854653cba750201e
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-03-02 17:33:44 +0100

    MerkleTree
    Implementing MerkleTree (WIP)

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -2,66 +2,44 @@
 
 // Base contract for whitelist
 
+import "../node_modules/@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 pragma solidity ^0.8.0;
 
 contract Whitelist{
 
-    event Whitelisted(address[] indexed _addresses, uint256 _allowance, uint256 _totalAllowance);
+    uint16 private whitelist_totalAllowance;
+    mapping(address => uint8) whitelist_claimed;
+    bytes32 public whitelist_merkleRoot;
 
-    mapping(address => uint256) private whitelistAllowance;
-    uint256 private whitelistTotalAllowance;
-
-    // Tests have demonstrated that a combinaton of for loop and explicitly repeated commands is the most efficient (and less gas-consuming) way to loop through thousands of addresses
-    // For ~2500 addresses, looping through 16 repeated lines has proven to give the best results
-    function batchWhitelist(address[] calldata _addresses, uint256 _allowance) internal virtual returns(bool){
-        uint256 remainder = _addresses.length%16;
-        for (uint256 index=0; index<(_addresses.length-remainder); index+=16){
-            whitelistAllowance[_addresses[index    ] ] = _allowance;
-            whitelistAllowance[_addresses[index + 1] ] = _allowance;
-            whitelistAllowance[_addresses[index + 2] ] = _allowance;
-            whitelistAllowance[_addresses[index + 3] ] = _allowance;
-            whitelistAllowance[_addresses[index + 4] ] = _allowance;
-            whitelistAllowance[_addresses[index + 5] ] = _allowance;
-            whitelistAllowance[_addresses[index + 6] ] = _allowance;
-            whitelistAllowance[_addresses[index + 7] ] = _allowance;
-            whitelistAllowance[_addresses[index + 8] ] = _allowance;
-            whitelistAllowance[_addresses[index + 9] ] = _allowance;
-            whitelistAllowance[_addresses[index +10] ] = _allowance;
-            whitelistAllowance[_addresses[index +11] ] = _allowance;
-            whitelistAllowance[_addresses[index +12] ] = _allowance;
-            whitelistAllowance[_addresses[index +13] ] = _allowance;
-            whitelistAllowance[_addresses[index +14] ] = _allowance;
-            whitelistAllowance[_addresses[index +15] ] = _allowance;
-        }
-        for (uint256 i=(_addresses.length-remainder); i<_addresses.length; i++){
-            whitelistAllowance[_addresses[i]] = _allowance;
-        }
-
-        whitelistTotalAllowance+=(_addresses.length * _allowance);
-
-        emit Whitelisted(_addresses, _allowance, whitelistTotalAllowance);
-        return true;
-    }
+    /*
+    functoin inspectMerkleTree (bytes32[] calldata merkleProof, bytes32 merkleRoot, address account){
+        require(whitelist_claimed[account] < 2, "Address already claimed");
+        bytes32 leaf = keccak256(abi.encode(_account));
+        require(MerkleProof.verify(_merkleProof, _merkleRoot, leaf), "Caller is not a claimer");
+    }*/
 
     // Get whitelist allowance of given address
-    function getWhitelistAllowance(address _address) view public returns(uint allowance){
-        allowance = whitelistAllowance[_address];
+    function getWhitelistAllowance(address _account, bytes32[] calldata _merkleProof, bytes32 _merkleRoot) view public returns(uint allowance){
+        if(whitelist_claimed[_account]>0) allowance = 2-whitelist_claimed[_account];
+        else{
+            bytes32 leaf = keccak256(abi.encode(_account));
+            allowance = ( MerkleProof.verify(_merkleProof, _merkleRoot, leaf) ? 2 : 0 );
+        }
     }
-
     // Get whitelist total allowance
     function getWhitelistTotalAllowance() view public returns(uint totalAllowance){
-        totalAllowance = whitelistTotalAllowance;
+        totalAllowance = whitelist_totalAllowance;
     }
-
-    // Reduce allowance
-    function _useWhitelistAllowance(address _address, uint256 _amount) internal virtual returns(bool){
-        if (_amount >= whitelistAllowance[_address]){
-            delete whitelistAllowance[_address];                                                          // Free up storage in exchange of gas discount
-            whitelistTotalAllowance-=whitelistAllowance[_address];                                        // Also update total allowance
+    // Use allowance
+    function _useWhitelistAllowance(address _address, uint8 _amount) internal virtual returns(bool){
+        if (_amount + whitelist_claimed[_address] > 1){
+            whitelist_totalAllowance -= (2-whitelist_claimed[_address]);
+            whitelist_claimed[_address]=2;
         }                      
         else {
-            whitelistAllowance[_address] -= _amount;
-            whitelistTotalAllowance -= _amount;
+            whitelist_claimed[_address] += _amount;
+            whitelist_totalAllowance -= _amount;
         }
         return true;
     }
