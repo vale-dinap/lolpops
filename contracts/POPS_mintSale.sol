873fd03e68f2efd7cb984e1f6ee962f6325e71ed
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-03-04 17:11:34 +0100

    Implemented merkleProof whitelist
    Implemented merkleProof whitelist functionality

--- contracts/POPS_mintSale.sol
+++ contracts/POPS_mintSale.sol
@@ -27,8 +27,6 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
 
     ///// CONTRACT VARIABLES /////
 
-    uint8 whitelistAllowance = 2;
-    uint8 whitelistValidity = 4;                                                                          // In days
     uint8 goldenTicketsValidity = 2;                                                                      // In days
     uint16 priceStep = 1800;                                                                              // Number of sales to increase the price
     uint256 public saleStart;                                                                             // Unix timestamp
@@ -59,10 +57,10 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
         _;
     }
     // Use whitelist functionality during its validity
-    modifier useWhitelist (address buyer, uint256 amount){
-        if(block.timestamp < (saleStart + (1 days * whitelistValidity) )){
-            require(amount < 1 + getWhitelistAllowance(buyer) + nonReserved(), "Request exceeds allowance (whitelist + non_reserved)");
-            _useWhitelistAllowance(buyer, amount);
+    modifier useWhitelist (address buyer, uint8 amount, bytes32[] calldata merkleProof){
+        if(block.timestamp < (saleStart + (1 days * whitelist_validity) )){
+            require(amount < 1 + getWhitelistAllowance(buyer, merkleProof) + nonReserved(), "Request exceeds allowance (whitelist + non_reserved)");
+            _useWhitelistAllowance(buyer, amount, merkleProof);
         }
         _;
     }
@@ -70,13 +68,13 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
 
     ///// FUNCTIONS - BEFORE SALE /////
 
-    // Adds a list of addresses to the whitelist - input as an array ["address1", "address2", ...]        // The functions to check whitelist allowance and
-    function addToWhitelist(address[] calldata _addresses) public onlyOwner returns(bool success){        // use it are already derived from the base contract
-        require(block.timestamp < saleStart, "Sale has already started - whitelist permanently locked");
-        batchWhitelist(_addresses, whitelistAllowance);
+    // [Tx][Public][Owner] Initialize whitelist
+    function whitelist_initialize(uint16 _whitelist_length, bytes32 _whitelist_merkleRoot) public onlyOwner returns(bool success){
+        super._whitelistInitialize(_whitelist_length, _whitelist_merkleRoot);
         success = true;
     }
-    // Start the sale
+
+    // [Tx][Public][Owner] Start the sale
     function startSale(uint256 _duration_days, address _goldenTicketContract) public onlyOwner returns(bool success){
         require(saleStart != 0, "Sale has already started");
         saleStart = block.timestamp;
@@ -85,28 +83,28 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
         success = true;
     }
 
-    
+
     ///// FUNCTIONS - DURING SALE /////
 
-    // Get current price
+    // [View][Public] Get current price
     function currentPrice() view public returns(uint256 price){
         price = priceCurve[ POPS_nft(POPS_address).totalSupply() / priceStep ];
     }
-    // Get available POPS
+    // [View][Public] Get available POPS
     function availablePOPS() view public returns(uint256 available){
         available= 10000 - POPS_nft(POPS_address).totalSupply();
     }
-    // Get non reserved POPS (WL + GT)
+    // [View][Private] Get non reserved POPS (WL + GT)
     function nonReserved() view private returns(uint256 amount){
         amount = availablePOPS();
         if(duringGTvalidity()) {amount -= GoldenTicket(POPS_goldenTicket).totalSupply();}
-        if(block.timestamp < (saleStart + (1 days * whitelistValidity))){ amount  -= getWhitelistTotalAllowance();}
+        if(block.timestamp < (saleStart + (1 days * whitelist_validity))){ amount  -= getWhitelistTotalAllowance();}
     }
-    // Check if currently during golden tickets validity
+    // [View][Public] Check if currently during golden tickets validity
     function duringGTvalidity() view public returns(bool result){
         result = (block.timestamp < (saleStart + (1 days * goldenTicketsValidity) ) );
     }
-    // Redeem golden tickets
+    // [Tx][Private] Redeem golden tickets
     function useGoldenTickets(address buyer, uint8 requested) private returns(uint8 credits){
         if ( duringGTvalidity() ){                                                                        // If during validity,
             uint8 available = uint8(GoldenTicket(POPS_goldenTicket).balanceOf(buyer));                    // check buyer's amount of tickets
@@ -115,12 +113,12 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
         }
         else{credits = 0;}                                                                                // Return zero credits if GT are expired
     }
-    // Main buy function
-    function buy(uint8 _amount, bool useGT) payable public onlyDuringSale whenNotPaused nonReentrant useWhitelist(msg.sender, _amount) returns(bool success){
+    // [Tx][Public] Main buy function
+    function buy(uint8 _amount, bool _useGT, bytes32[] calldata _whitelist_merkleProof) payable public onlyDuringSale whenNotPaused nonReentrant useWhitelist(msg.sender, _amount, _whitelist_merkleProof) returns(bool success){
         require(_amount > 0 && _amount < 11, "You can mint 1 to 10 POPS at once");
         require(_amount < 1+availablePOPS(), "Mint would exceed max supply");
         uint8 credits;
-        if (useGT) credits = useGoldenTickets(msg.sender, _amount);                                       // Spend golden tickets
+        if (_useGT) credits = useGoldenTickets(msg.sender, _amount);                                       // Spend golden tickets
         uint256 salePrice = currentPrice()*(_amount - credits);                                           // Calculate sale price
         require(msg.value >= salePrice, "Please make sure to send enough eth");
         (bool paid, ) = POPS_teamWallet.call{value: address(this).balance}("");                           // Forward ether to the teamWallet address. By sending contract balance instead of msg.value, we ensure that also value that previously got stuck in the contract due to unforseen circumstances is sent, for the same gas cost
@@ -142,12 +140,12 @@ contract POPSsale is Ownable, Pausable, ReentrancyGuard, Whitelist, MintingRando
     }
     */
 
-    // Pause the contract
+    // [Tx][Public][Owner] Pause the contract
     function pauseContract() public onlyOwner {
         _pause();
     }
 
-    // Unpause the contract
+    // [Tx][Public][Owner] Unpause the contract
     function unpauseContract() public onlyOwner {
         _unpause();
     }
