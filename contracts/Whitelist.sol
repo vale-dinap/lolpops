db120884cb0aaf1c76232210e428e02bb9030026
vale-dinap <69582234+vale-dinap@users.noreply.github.com>
2022-03-04 17:10:46 +0100

    Fully implemented merkleProoof
    Implemented merkleProof functionality

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -8,39 +8,46 @@ pragma solidity ^0.8.0;
 
 contract Whitelist{
 
-    uint16 private whitelist_totalAllowance;
-    mapping(address => uint8) whitelist_claimed;
-    bytes32 public whitelist_merkleRoot;
+    uint8 internal whitelist_validity = 4;                                                                // In days
+    uint8 whitelist_allowance = 2;                                                                        // Allowance per address
+    uint16 private whitelist_totalAllowance;                                                              // Total allowance of whitelist, set via constructor
+    mapping(address => uint8) whitelist_claimed;                                                          // Tracks whitelist addresses that have already claimed
+    bytes32 public whitelist_merkleRoot;                                                                  // MerkleRoot of the whitelist, set via constructor
 
-    /*
-    functoin inspectMerkleTree (bytes32[] calldata merkleProof, bytes32 merkleRoot, address account){
-        require(whitelist_claimed[account] < 2, "Address already claimed");
-        bytes32 leaf = keccak256(abi.encode(_account));
-        require(MerkleProof.verify(_merkleProof, _merkleRoot, leaf), "Caller is not a claimer");
-    }*/
+    ///// FUNCTIONS /////
 
-    // Get whitelist allowance of given address
-    function getWhitelistAllowance(address _account, bytes32[] calldata _merkleProof, bytes32 _merkleRoot) view public returns(uint allowance){
+    // [Tx][Internal] Initialize whitelist - NOTE: restrict to owner before exposing
+    function _whitelistInitialize (uint16 _whitelist_length, bytes32 _whitelist_merkleRoot) internal returns (bool success){
+        whitelist_totalAllowance = _whitelist_length*whitelist_allowance;
+        whitelist_merkleRoot = _whitelist_merkleRoot;
+        success=true;
+    }
+
+    // [View][Public] Get whitelist allowance of given address
+    function getWhitelistAllowance(address _account, bytes32[] calldata _merkleProof) view public returns(uint8 allowance){
         if(whitelist_claimed[_account]>0) allowance = 2-whitelist_claimed[_account];
         else{
             bytes32 leaf = keccak256(abi.encode(_account));
-            allowance = ( MerkleProof.verify(_merkleProof, _merkleRoot, leaf) ? 2 : 0 );
+            allowance = ( MerkleProof.verify(_merkleProof, whitelist_merkleRoot, leaf) ? whitelist_allowance : 0 );
         }
     }
-    // Get whitelist total allowance
+
+    // [View][Public] Get whitelist total allowance
     function getWhitelistTotalAllowance() view public returns(uint totalAllowance){
         totalAllowance = whitelist_totalAllowance;
     }
-    // Use allowance
-    function _useWhitelistAllowance(address _address, uint8 _amount) internal virtual returns(bool){
-        if (_amount + whitelist_claimed[_address] > 1){
-            whitelist_totalAllowance -= (2-whitelist_claimed[_address]);
-            whitelist_claimed[_address]=2;
-        }                      
-        else {
-            whitelist_claimed[_address] += _amount;
-            whitelist_totalAllowance -= _amount;
+
+    // [Tx][Internal] Use whitelist allowance
+    function _useWhitelistAllowance(address _address, uint8 _amount, bytes32[] calldata _merkleProof) internal virtual returns(uint8 used){
+        uint8 addressAllowance;
+        if(whitelist_claimed[_address]>0) addressAllowance = 2-whitelist_claimed[_address];
+        else addressAllowance = getWhitelistAllowance(_address, _merkleProof);
+        if(addressAllowance>0){
+            if (_amount < addressAllowance) used = addressAllowance - _amount;                     
+            else used = addressAllowance;
+            whitelist_totalAllowance -= used;
+            whitelist_claimed[_address] += used;
         }
-        return true;
+        else used=0;
     }
 }
